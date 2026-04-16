5cb47638ca7302cf4abd958a31c3bfcaf2b6b259
GeovasDev <badas8863@gmail.com>
2026-04-16 15:24:04 -0300

    Fix: Removido sitemap integration para corrigir falha no build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://jeovadesousasilva.github.io',
-	base: '/GameDevIndie',
-	integrations: [mdx(), sitemap()],
+	site: 'https://jeovadesousasilva.github.io/GameDevIndie/',
+	integrations: [mdx()],
 });
