273e9bd4eaf9ca96ccc9f59686315f821ff6cd19
GeovasDev <badas8863@gmail.com>
2026-04-16 15:14:54 -0300

    Deploy: Adicionado GitHub Actions para publicação automática

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://jeovadesousasilva.github.io/GameDevIndie',
+	site: 'https://jeovadesousasilva.github.io',
+	base: '/GameDevIndie',
 	integrations: [mdx(), sitemap()],
 });
